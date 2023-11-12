d87e4a1ea2981b5507df571a18dffa79a1f10d62
João Henrique <joaohnsousa@gmail.com>
2023-11-12 11:35:06 -0300

    Mundando as telas de Jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
-import './App.css'
-import StartScreen from './Components/StartScreen/StartScreen'
-import { useCallback, useEffect, useState } from 'react'
-import { wordsList } from './data/words'
-import Game from './Components/game/Game'
-import End from './Components/end/End'
+import "./App.css";
+import StartScreen from "./Components/StartScreen/StartScreen";
+import { useCallback, useEffect, useState } from "react";
+import { wordsList } from "./data/words";
+import Game from "./Components/game/Game";
+import End from "./Components/end/End";
 
 function App() {
-
   const stages = [
-    {id: 1, name: "start"},
-    {id: 2, name: "game"},
-    {id: 3, name: "end"},
-  ]
-
+    { id: 1, name: "start" },
+    { id: 2, name: "game" },
+    { id: 3, name: "end" },
+  ];
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
 
-  const [gameStage, setGameStage] = useState(stages[0].name);
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <>
-    <div className="App">
-      {gameStage === "start" && <StartScreen/> }
-      {gameStage === "game" && <Game/> }
-      {gameStage === "end" && <End/> }
-    </div>
-      
+      <div className="App">
+        {gameStage === "start" && <StartScreen startGame={startGame} />}
+        {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+        {gameStage === "end" && <End retry={retry}/>}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/end/End.jsx
+++ src/Components/end/End.jsx
@@ -1,9 +1,10 @@
 import "./End.css"
 
-const End = () => {
+const End = ({retry}) => {
   return (
     <div>
-      
+      <h1>End</h1>
+      <button onClick={retry}>Reniciar jogo</button>
     </div>
   )
 }
